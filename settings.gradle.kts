195f47db244b4c9d1e9f464cfe09eb194f51abe9
Akmal <akmalrustamovich0406@gmail.com>
2025-12-15 22:34:09 +0500

    update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,9 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    }
 }
 
 dependencyResolutionManagement {
